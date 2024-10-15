66812b60ca4f275ec23e0b93dbd8d40f07daa47c
AidarBahrazov <aidauaklass@gmail.com>
2024-10-15 22:07:04 +0500

    homework4

--- homework4.py
+++ homework4.py
@@ -1,9 +1,3 @@
-def convert(n):
-    if n < 4:
-        return str(n)
-    else:
-        return convert(n // 4) + str(n % 4)
-
 
 a = input("Введите целое простое число a: ")
 b = input("Введите целое простое число b (b не должно быть равно 0): ")
@@ -17,8 +11,7 @@ if a.isdigit() and b.isdigit() and c.isdigit():
     if b != 0:
         result = a / b - 2 * c
         print(f"Результат в десятичной системе: {result}")
-        print(f"Результат в четверичной системе: {convert(int(result))}")
     else:
         print("Ошибка: b не должно быть равно 0.")
 else:
-    print("Ошибка: введите корректные целые числа.")
+    print("Ошибка: введите корректные целые числа")
