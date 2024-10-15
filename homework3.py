4ce27c7d6c6423e6646562d5f29d6ddd52b13e01
AidarBahrazov <aidauaklass@gmail.com>
2024-10-16 00:44:45 +0500

    изменил ограничения homework3.py

--- homework3.py
+++ homework3.py
@@ -4,10 +4,10 @@ def convert(n):
     else:
         return convert(n // 4) + str(n % 4)
 
-number = input("Введите целое простое десятичное число: ")
+number = input("Введите целое положительное простое десятичное число: ")
 
 if number.isdigit():
     number = int(number)
     print(f"Четверичное представление: {convert(number)}")
 else:
-    print("Ошибка: введите корректное целое число.")
+    print("Ошибка: введите корректное положительное целое число.")
